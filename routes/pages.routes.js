0d392135790beb308beb1ea3ccc84a63bbc970e2
suat.ogme <suat.ogme@gmail.com>
2021-08-21 21:41:59 +0300

    Dev dependencies updated

--- routes/pages.routes.js
+++ routes/pages.routes.js
@@ -2,7 +2,7 @@ const express = require("express");
 const router = express.Router();
 const axios = require('axios');
 
-async function getEpisodesById(id) {
+async function getCharacterById(id) {
     const response = await axios.get(`${process.env.API}/character/${id}`);
     return response.data;
 }
@@ -15,13 +15,16 @@ router.get('/', (req, res, next) => {
     next()
 });
 
-router.get('/about', (req, res, next) => {
-    getEpisodesById(1).then(data => {
-        const episodes = [...data.episode].map(episode => {
+router.get('/characterepisodes', (req, res, next) => {
+    getCharacterById(1).then(data => {
+        const episodesIdlist = [...data.episode].map(episode => {
             let episodeId = episode.replace(/[\w].+\//g, '');
             return episodeId;
         })
-        res.send(episodes);
+        res.send({
+            episodesIdlist: episodesIdlist,
+            episodes: data.episode
+        });
     });
 });
 
