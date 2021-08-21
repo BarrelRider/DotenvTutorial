529632a943aa6e42f00307a9d4c764e0b5bdb6f3
suat.ogme <suat.ogme@gmail.com>
2021-08-21 20:31:24 +0300

    Initial commit

---
+++ routes/pages.routes.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const router = express.Router();
+const axios = require('axios');
+
+async function getEpisodesById(id) {
+    const response = await axios.get(`https://rickandmortyapi.com/api/character/${id}`);
+    return response.data;
+}
+
+router.get('/', (req, res, next) => {
+    res.send({
+        path: "root",
+        env: process.env.SECRET_KEY
+    });
+    next()
+});
+
+router.get('/about', (req, res, next) => {
+    getEpisodesById(1).then(data => {
+        const episodes = [...data.episode].map(episode => {
+            let episodeId = episode.replace(/[\w].+\//g, '');
+            return episodeId;
+        })
+        res.send(episodes);
+    });
+});
+
+
+
+module.exports = router;
---
+++ server.js
@@ -0,0 +1,24 @@
+const express = require("express");
+const cors = require("cors");
+const path = require("path");
+const pagesRoute = require('./routes/pages.routes');
+
+require('dotenv').config({
+    path: path.join(__dirname, `.env/.env.${process.env.NODE_ENV}`)
+});
+
+const app = express();
+app.use(cors());
+app.use(express.json());
+app.disable('etag');
+
+
+app.use('/', pagesRoute);
+
+app.all('*', (req, res) => {
+    res.status(404).send("<h1>404 Not Found</h1>")
+})
+
+app.listen(process.env.PORT || 3000, () => {
+    console.log('Server is listening on', process.env.PORT || 3000)
+});
