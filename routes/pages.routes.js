b18a80454edae80d90a7d911a5eaf440849361c2
aykutmayali <aykutmayali@gmail.com>
2021-08-22 23:31:11 +0300

    scripts folder created

--- routes/pages.routes.js
+++ routes/pages.routes.js
@@ -2,38 +2,10 @@ const express = require("express");
 const router = express.Router();
 const axios = require('axios');
 const Character = require('../models/character');
-
-async function getCharacterById(id) {
-    const response = await axios.get(`${process.env.API}/character/${id}`);
-    const episodesIdlist = [...response.data.episode].map(episode => {
-        let episodeId = episode.replace(/[\w].+\//g, '');
-        return episodeId;
-    });
-    
-    const episodeList = await getCharacterEpisodes(episodesIdlist);
-    return {
-        ...response.data,
-        episodeList
-    };
-}
-
-async function getCharacterEpisodes(idList) {
-    const response = await axios.get(`${process.env.API}/episode/${idList}`)
-    return response.data;
-}
-
-async function getCharacters() {
-    const response = await axios.get(`${process.env.API}/character`);
-    return response.data;
-}
-
-// async function getEpisodes() {
-//     const response = await axios.get(`${process.env.API}/episode`);
-//     return response.data
-// }
+const characterMgr = require('./scripts/getCharacter');
 
 router.get('/', (req, res, next) => {
-    getCharacters().then(data => {
+    characterMgr.getCharacters().then(data => {
         let characters = [...data.results];
         res.render('index', {
             characters
@@ -44,7 +16,7 @@ router.get('/', (req, res, next) => {
 
 router.get('/detail/character/:id', (req, res, next) => {
     const id = req.params.id;
-    getCharacterById(id).then(data => {
+    characterMgr.getCharacterById(id).then(data => {
         const character = new Character(data);
         
         character.episodes = data.episodeList.length > 0 ? [...data.episodeList] : [data.episodeList];
